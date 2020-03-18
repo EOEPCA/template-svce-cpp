7693512a1f9f26616836b6c71401e7d2fcd7b311
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-03-18 16:22:09 +0100

    pre-build fixed

--- build-release/pre-build.sh
+++ build-release/pre-build.sh
@@ -1,1 +1,1 @@
-cmake -G "Unix Makefiles" --build "build-release"  -DCMAKE_BUILD_TYPE=Release  -DYAML_CPP_BUILD_TESTS=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -G "Unix Makefiles" --build "build-release"  -DCMAKE_BUILD_TYPE=Release  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
