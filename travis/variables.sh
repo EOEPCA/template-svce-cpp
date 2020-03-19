20e240d9ec0f2455a02daadbc84fe4817d067a3b
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-03-19 14:21:35 +0100

    script updated

--- travis/variables.sh
+++ travis/variables.sh
@@ -3,8 +3,17 @@
 export NULL='none'
 export LOCAL_DOCKERIMAGE='eoepca/eoepca-build'
 export EOEPCA_IMAGE="eoepca-build"
-export TRAVIS_BRANCH="${TRAVIS_BRANCH:-develop}"
+TRAVIS_BRANCH="${TRAVIS_BRANCH:-develop}"
 export TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-0}"
+
+echo ${TRAVIS_BRANCH} | grep '/'
+if [ $? -eq 0 ]
+then
+	TRAVIS_BRANCH=$(echo ${TRAVIS_BRANCH}| sed -e 's@/@_@g')
+fi
+
+export TRAVIS_BRANCH
+
 export buildTag=travis_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}
 export BUILD_IMAGE="${BUILD_IMAGE:-${NULL}}"
 export BUILD_IMAGE_PUSH="${BUILD_IMAGE_PUSH:-${NULL}}"
