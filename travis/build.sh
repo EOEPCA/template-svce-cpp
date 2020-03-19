57f1db11e0a7da740af04748f45fbde60f62f513
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-03-19 15:48:52 +0100

    scripts updated

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#CMAKE prepare
+docker run --rm -ti  -v $PWD:/project/ -w /project/build/  ${LOCAL_DOCKERIMAGE} cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ..
 
+#make
+docker run --rm -ti  -v $PWD:/project/ -w /project/build/  ${LOCAL_DOCKERIMAGE} make
 
-
+#tests
+docker run --rm -ti  -v $PWD:/project/ -w /project/build/  ${LOCAL_DOCKERIMAGE} ./test/eoepca-test
