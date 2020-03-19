2fd497945b4132fb44cf7e524e87daef3e779d07
Roberto  di Rienzo <roberto.dirienzo@terradue.com>
2020-03-19 11:06:58 +0100

    Docker image build

--- travis/containerCreation.sh
+++ travis/containerCreation.sh
@@ -3,17 +3,14 @@
 # fail fast settings from https://dougrichardson.org/2018/08/03/fail-fast-bash-scripting.html
 set -euov pipefail
 
-
 # Check presence of environment variables
 TRAVIS_BRANCH="${TRAVIS_BRANCH:-develop}"
 TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-0}"
 
 # Create a Docker image and tag it as 'travis_<build number>'
-buildTag=travis_${TRAVIS_BRANCH}_$TRAVIS_BUILD_NUMBER
-
 docker build --rm -t eoepca/testcompiler .
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $DOCKER_USERNAME/testcompiler:1   # defaults to docker hub
 
-docker push $DOCKER_USERNAME/testcompiler:$buildTag   # defaults to docker hub
 
